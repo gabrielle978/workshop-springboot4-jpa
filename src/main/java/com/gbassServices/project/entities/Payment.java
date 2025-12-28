2797846737cbfa4f32649de498ce58e5172f3d77
gabrielle978 <gbassiquete@outlook.com>
2025-12-27 21:40:13 -0300

    Subtotal & Total methods

--- src/main/java/com/gbassServices/project/entities/Payment.java
+++ src/main/java/com/gbassServices/project/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.gbassServices.project.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -20,6 +21,8 @@ public class Payment implements Serializable {
 
     @OneToOne
     @MapsId
+    @JsonIgnore
+
     private Order order;
 
     public Payment(){
