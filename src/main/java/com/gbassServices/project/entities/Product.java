7ad7e5ba6dd20ca408d5dbaef8de31325bd3e6a8
gabrielle978 <gbassiquete@outlook.com>
2025-12-27 21:05:12 -0300

    Product-OrdersItem one-to-many association

--- src/main/java/com/gbassServices/project/entities/Product.java
+++ src/main/java/com/gbassServices/project/entities/Product.java
@@ -1,5 +1,6 @@
 package com.gbassServices.project.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -27,6 +28,9 @@ public class Product implements Serializable {
                 inverseJoinColumns = @JoinColumn(name = "category_id")
                 )
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany (mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     //Constructor empty
     public Product (){
@@ -84,6 +88,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
     }
 
     //Hashcode and equals
